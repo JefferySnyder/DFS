b3e30957ba26ceef5c374ee442436ffb186ec0fa
jeff.snyder <jeff.snyder@bgi-llc.com>
2026-06-12 14:21:57 -0700

    Add download file function to client

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,7 +5,8 @@ using System.Text.Json;
 using System.Threading.Tasks;
 
 using var httpClient = new HttpClient();
-const int BlockSize = 1024 * 1024; // 1 MB Blocks
+//const int BlockSize = 1024 * 1024; // 1 MB Blocks
+const int BlockSize = 1024; // 1 KB Blocks
 const string CoordinatorUrl = "http://localhost:5000";
 
 // --- WRITE PATH ---
@@ -37,7 +38,36 @@ async Task UploadFileAsync(string localPath, string dfsFileName)
     Console.WriteLine("Distributed upload complete.");
 }
 
-await UploadFileAsync("", "");
+async Task RetrieveFileAsync(string dfsFileName, string storePath)
+{
+    // ask coordinator for block IDs associated with file
+    var response = await httpClient.GetAsync($"{CoordinatorUrl}/files/lookup?fileName={dfsFileName}");
+    var planJson = await response.Content.ReadAsStringAsync();
+    var blockIds = JsonSerializer.Deserialize<string[]>(planJson, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+    if (blockIds is null)
+    {
+        Console.WriteLine("Failed to retrieve block IDs");
+        return;
+    }
 
+    const string ReadDataNodeUrl = "http://localhost:5001";
+    // Retrieve data from nodes
+    for (int i = 0; i < blockIds.Length; i++)
+    {
+        var filePath = Path.Combine(storePath, blockIds[i]);
+        using var downloadStream = await httpClient.GetStreamAsync($"{ReadDataNodeUrl}/blocks/{blockIds[i]}");
+        using var fileStream = File.Create(filePath);
+        await downloadStream.CopyToAsync(fileStream);
+    }
+    Console.WriteLine("Distributed donwload complete.");
+}
+
+const string LocalPath = "C:/Users/Jeffery.Snyder/Documents/output.txt";
+const string DfsFileName = "output.txt";
+const string StorePath = "C:/Users/Jeffery.Snyder/Documents/output";
+
+await UploadFileAsync(LocalPath, DfsFileName);
+
+await RetrieveFileAsync(DfsFileName, StorePath);
 public record BlockAssignment(string BlockId, string NodeUrl);
 
