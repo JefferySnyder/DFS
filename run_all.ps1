92a0a8c301359417d04d977b6b1d76ab60a36cd2
jeff.snyder <jeff.snyder@bgi-llc.com>
2026-06-15 10:06:10 -0700

    Add powershell script to automate running projects

---
+++ run_all.ps1
@@ -0,0 +1,13 @@
+$projects = @(
+	"C:\Users\Jeffery.Snyder\source\others\DFS\Coordinator\Coordinator.csproj"
+	"C:\Users\Jeffery.Snyder\source\others\DFS\DataNode\DataNode.csproj"
+	"C:\Users\Jeffery.Snyder\source\others\DFS\Client\Client.csproj"
+)
+
+# Launch each project in its own console window
+foreach ($project in $projects) {
+    Write-Host "Launching background window for: $project" -ForegroundColor Cyan
+    Start-Process powershell -ArgumentList "-NoExit", "-Command", "dotnet run --project $project"
+	Start-Sleep -Seconds 5
+}
+
